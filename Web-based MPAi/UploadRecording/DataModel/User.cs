5f50b77e49dac7782086234c6f3430025c184fa5
Jayden Cooke <JaydenCooke@outlook.com>
2017-05-09 16:10:02 +1200

    Added fields to user class

--- Web-based MPAi/UploadRecording/DataModel/User.cs
+++ Web-based MPAi/UploadRecording/DataModel/User.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UploadRecording.DataModel
 {
-    public class User
+    [Table("User")]
+    public partial class User
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        [Index(IsUnique = true)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Password { get; set; }
     }
 }
