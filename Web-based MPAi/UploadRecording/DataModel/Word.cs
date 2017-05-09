26a381e9058b2885ece9edd37d584122fdd41566
Jayden Cooke <JaydenCooke@outlook.com>
2017-05-09 17:35:55 +1200

    Added fields to Word class

--- Web-based MPAi/UploadRecording/DataModel/Word.cs
+++ Web-based MPAi/UploadRecording/DataModel/Word.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UploadRecording.DataModel
 {
-    public class Word
+    [Table("Word")]
+    public partial class Word
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int WordId { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Recording> Recordings { get; set; }
     }
 }
