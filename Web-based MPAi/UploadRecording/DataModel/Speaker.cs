d7e62a27f820425d87036623df3890a5cded3ea1
Jayden Cooke <jaydencooke@outlook.com>
2017-07-12 11:42:25 +1200

    Add default speaker enum value
    Rather than using null, which can throw exceptions, I've added an
    UNIDENTIFIED value to the speaker enum to use as a default value.
    Also commented the classs to point this out.

--- Web-based MPAi/UploadRecording/DataModel/Speaker.cs
+++ Web-based MPAi/UploadRecording/DataModel/Speaker.cs
@@ -5,8 +5,12 @@ using System.Web;
 
 namespace UploadRecording.DataModel
 {
+    /// <summary>
+    /// Enum representing the speaker of a recording. 
+    /// Unidentified speakers will not be used when a specific speaker is requested, but will still show without error when all words are listed.
+    /// </summary>
     public enum Speaker
     {
-        KAUMATUA_MALE, KUIA_FEMALE, MODERN_MALE, MODERN_FEMALE
+        KAUMATUA_MALE, KUIA_FEMALE, MODERN_MALE, MODERN_FEMALE, UNIDENTIFIED
     }
 }
