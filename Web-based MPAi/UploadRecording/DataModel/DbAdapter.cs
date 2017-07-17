332c04f50127b08a7931740634670ea2138f8970
Jayden Cooke <jaydencooke@outlook.com>
2017-07-17 14:06:53 +1200

    Add simple database adapter class
    This allows the underlying database to be called from other classes
    consistently, irrespective of the implementation of the database.

---
+++ Web-based MPAi/UploadRecording/DataModel/DbAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UploadRecording.DataModel
+{
+    public class DbAdapter
+    {
+        private MPAiContext context;
+
+        public DbAdapter(MPAiContext context)
+        {
+            this.context = context;
+        }
+    }
+}
