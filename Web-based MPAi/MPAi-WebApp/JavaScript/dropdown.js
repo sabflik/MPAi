abbcc6ec814319aa26d0beef40b9d7da337f8ee7
sabflik <szaf045@aucklanduni.ac.nz>
2017-07-23 15:40:24 +1200

    Change dropdown to receive array

--- Web-based MPAi/MPAi-WebApp/JavaScript/dropdown.js
+++ Web-based MPAi/MPAi-WebApp/JavaScript/dropdown.js
@@ -1,7 +1,5 @@
 $(function () {
 
-    var words;
-
     // Create a new XMLHttpRequest.
     var request = new XMLHttpRequest();
 
@@ -10,7 +8,27 @@ $(function () {
         if (request.readyState === 4) {
             if (request.status === 200) {
                 // Parse the JSON
-                words = JSON.parse(request.responseText);
+                var words = JSON.parse(request.responseText);
+
+                $('#maoriWord').autoComplete({
+                    minChars: 0,
+                    source: function (term, suggest) {
+                        term = term.toLowerCase();
+
+                        var suggestions = [];
+                        for (i = 0; i < words.length; i++)
+                            if (~(words[i]).toLowerCase().indexOf(term)) suggestions.push(words[i]);
+                        suggest(suggestions);
+                    },
+                    renderItem: function (item, search) {
+                        search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+                        var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+                        return '<div class="autocomplete-suggestion" data-word="' + item + '" data-val="' + search + '"> ' + item.replace(re, "<b>$1</b>") + '</div>';
+                    },
+                    onSelect: function (e, term, item) {
+                        $('#maoriWord').val(item.data('word'));
+                    }
+                });
             }
         }
     };
@@ -18,24 +36,4 @@ $(function () {
     // Set up and make the request.
     request.open('GET', 'Dropdown.aspx', true);
     request.send();
-
-    $('#maoriWord').autoComplete({
-        minChars: 0,
-        source: function (term, suggest) {
-            term = term.toLowerCase();
-
-            var suggestions = [];
-            for (i = 0; i < words.length; i++)
-                if (~(words[i]).toLowerCase().indexOf(term)) suggestions.push(words[i]);
-            suggest(suggestions);
-        },
-        renderItem: function (item, search) {
-            search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-            var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
-            return '<div class="autocomplete-suggestion" data-word="' + item + '" data-val="' + search + '"> ' + item.replace(re, "<b>$1</b>") + '</div>';
-        },
-        onSelect: function (e, term, item) {
-            $('#maoriWord').val(item.data('word'));
-        }
-    });
 });
