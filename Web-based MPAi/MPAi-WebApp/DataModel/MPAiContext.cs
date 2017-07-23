7f2d813403f11843cabbe5e22fbfa36c28393a3a
Jayden Cooke <jaydencooke@outlook.com>
2017-07-23 15:26:12 +1200

    Adjust database values to resolve connection string issues
    This also reduces the amount fo hard-coding in the system.

--- Web-based MPAi/MPAi-WebApp/DataModel/MPAiContext.cs
+++ Web-based MPAi/MPAi-WebApp/DataModel/MPAiContext.cs
@@ -25,7 +25,7 @@ namespace MPAi_WebApp.DataModel
             }
         }
 
-        public MPAiContext() : base("name=MPAiModel")
+        public MPAiContext() : base("name=DefaultConnection")
         {
 
             if (!Directory.Exists(System.IO.Path.Combine(FilePath, "Database")))
@@ -117,7 +117,7 @@ namespace MPAi_WebApp.DataModel
                 newWord = WordSet.SingleOrDefault(x => x.Name.Equals(newWord.Name));
                 newRecording = new Recording()
                 {
-                    FilePath = filePath,
+                    FilePath = Path.Combine(@"Audio", Path.GetFileName(filePath)),
                     Speaker = speaker,
                     Word = newWord
                 };
@@ -134,7 +134,7 @@ namespace MPAi_WebApp.DataModel
     public class MPAiContextInitializer : CreateDatabaseIfNotExists<MPAiContext>
     {
         //private string AudioFolder = "C:\\Users\\adm.Jayden\\Work Folders\\Documents\\GitHub\\SabFlik\\MPAi\\Web-based MPAi\\MPAi-WebApp\\Audio";
-        private string AudioFolder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Audio");
+        private string AudioFolder = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"Audio");
         /// <summary>
         /// If the database doesn't exist, it is created.
         /// If it does exist, and the Audio folder a) exists and b) contains at least one .wav file, then
