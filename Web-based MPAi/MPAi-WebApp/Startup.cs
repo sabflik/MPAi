2f7399172ee08053d63f055ba421ec22faa03ec3
Jayden Cooke <jaydencooke@outlook.com>
2017-07-24 19:03:16 +1200

    Configure application startup to create the database if it does not already exist

--- Web-based MPAi/MPAi-WebApp/Startup.cs
+++ Web-based MPAi/MPAi-WebApp/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Owin;
+using MPAi_WebApp.DataModel;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(MPAi_WebApp.Startup))]
@@ -7,6 +8,11 @@ namespace MPAi_WebApp
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
+            using(MPAiContext context = MPAiContext.InitializeDBModel())
+            {
+                // Force the database to be seeded on startup
+                context.Database.Initialize(true);
+            }
         }
     }
 }
