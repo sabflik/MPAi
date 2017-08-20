5113b818feb5ef7ac2180cbe1c68cdcbf23d4433
Jayden Cooke <jaydencooke@outlook.com>
2017-08-20 18:02:58 +1200

    Adjust MPAiSQLite class to use using paradigm

--- Web-based MPAi/MPAi-WebApp/DataModel/MPAiSQLite.cs
+++ Web-based MPAi/MPAi-WebApp/DataModel/MPAiSQLite.cs
@@ -9,15 +9,12 @@ namespace MPAi_WebApp.DataModel
 {
     public class MPAiSQLite
     {
-        SQLiteConnection connection;
         public MPAiSQLite()
         {
-            if (!(System.IO.File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"MPAiDb.sqlite"))))
+            if (!(File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"MPAiDb.sqlite"))))
             {
                 SQLiteConnection.CreateFile(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MPAiDb.sqlite"));
             }
-            connection = new SQLiteConnection("Data Source="+Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MPAiDb.sqlite") +"; Version=3;");
-            connection.Open();
             createTables();
             populatetables();
         }
@@ -65,37 +62,40 @@ namespace MPAi_WebApp.DataModel
                     Speaker speaker = SpeakerFromFile(fileName);
 
                     // Create the word if it doesn't exist, get the name from the filename.
-
-                    string sql = "select count(*) from Word " +
-                        "where wordName = '" + fileName + "'";
-                    SQLiteCommand command = new SQLiteCommand(sql, connection);
-                    int count = Int32.Parse(command.ExecuteScalar().ToString());
-                    if (count <= 0)
+                    using (SQLiteConnection connection = new SQLiteConnection("Data Source=" + Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MPAiDb.sqlite") + "; Version=3;"))
                     {
-                        sql = "insert into Word (wordName)" +
-                            "values('" + fileName +
-                            "')";
+                        connection.Open();
+                        string sql = "select count(*) from Word " +
+                            "where wordName = '" + fileName + "'";
+                        SQLiteCommand command = new SQLiteCommand(sql, connection);
+                        int count = Int32.Parse(command.ExecuteScalar().ToString());
+                        if (count <= 0)
+                        {
+                            sql = "insert into Word (wordName)" +
+                                "values('" + fileName +
+                                "')";
+                            command = new SQLiteCommand(sql, connection);
+                            command.ExecuteNonQuery();
+                        }
+
+                        // Create the recording if it doesn't exist, associate it with the Word.
+
+                        sql = "select count(*) from Recording " +
+                            "where filePath = '" + fInfo.FullName + "'";
                         command = new SQLiteCommand(sql, connection);
-                        command.ExecuteNonQuery();
-                    }
+                        count = Int32.Parse(command.ExecuteScalar().ToString());
+                        if (count <= 0)
+                        {
+                            sql = "select wordId from Word " +
+                           "where wordName = '" + fileName + "'";
+                            command = new SQLiteCommand(sql, connection);
+                            int wordID = Int32.Parse(command.ExecuteScalar().ToString());
 
-                    // Create the recording if it doesn't exist, associate it with the Word.
-
-                    sql = "select count(*) from Recording " +
-                        "where filePath = '" + fInfo.FullName + "'";
-                    command = new SQLiteCommand(sql, connection);
-                    count = Int32.Parse(command.ExecuteScalar().ToString());
-                    if (count <= 0)
-                    {
-                        sql = "select wordId from Word " +
-                       "where wordName = '" + fileName + "'";
-                        command = new SQLiteCommand(sql, connection);
-                        int wordID = Int32.Parse(command.ExecuteScalar().ToString());
-
-                        sql = "insert into Recording(speaker, wordId, filePath) " +
-                            "values(" + Convert.ToInt32(speaker).ToString() +
-                            ", " + wordID.ToString() +
-                            ", '" + fInfo.FullName + "')";
+                            sql = "insert into Recording(speaker, wordId, filePath) " +
+                                "values(" + Convert.ToInt32(speaker).ToString() +
+                                ", " + wordID.ToString() +
+                                ", '" + fInfo.FullName + "')";
+                        }
                     }
                 }
             }
@@ -103,41 +103,45 @@ namespace MPAi_WebApp.DataModel
 
         void createTables()
         {
-            string sql = "create table if not exists Word(" +
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=" + Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MPAiDb.sqlite") + "; Version=3;"))
+            {
+                connection.Open();
+                string sql = "create table if not exists Word(" +
                 "wordId integer primary key," +
                 "wordName text unique not null" +
                 ")";
-            SQLiteCommand command = new SQLiteCommand(sql, connection);
-            command.ExecuteNonQuery();
+                SQLiteCommand command = new SQLiteCommand(sql, connection);
+                command.ExecuteNonQuery();
 
-            sql = "create table if not exists User(" +
-                "userId integer primary key," +
-                "username text unique not null" +
-                ")";
-            command = new SQLiteCommand(sql, connection);
-            command.ExecuteNonQuery();
+                sql = "create table if not exists User(" +
+                    "userId integer primary key," +
+                    "username text unique not null" +
+                    ")";
+                command = new SQLiteCommand(sql, connection);
+                command.ExecuteNonQuery();
 
-            sql = "create table if not exists Score(" +
-                "scoreId integer primary key," +
-                "wordId integer, " +
-                "userId integer, " +
-                "percentage integer not null, " +
-                "date text not null, " +
-                "foreign key(wordId) references Word(wordId)," +
-                "foreign key(userId) references User(userId)" + 
-                ")";
-            command = new SQLiteCommand(sql, connection);
-            command.ExecuteNonQuery();
+                sql = "create table if not exists Score(" +
+                    "scoreId integer primary key," +
+                    "wordId integer, " +
+                    "userId integer, " +
+                    "percentage integer not null, " +
+                    "date text not null, " +
+                    "foreign key(wordId) references Word(wordId)," +
+                    "foreign key(userId) references User(userId)" +
+                    ")";
+                command = new SQLiteCommand(sql, connection);
+                command.ExecuteNonQuery();
 
-            sql = "create table if not exists Recording(" +
-                "recordingId integer primary key, " +
-                "speaker integer not null, " +
-                "wordId integer, " +
-                "filePath text unique not null, " +
-                "foreign key(wordId) references Word(wordId) " +
-                ")";
-            command = new SQLiteCommand(sql, connection);
-            command.ExecuteNonQuery();
+                sql = "create table if not exists Recording(" +
+                    "recordingId integer primary key, " +
+                    "speaker integer not null, " +
+                    "wordId integer, " +
+                    "filePath text unique not null, " +
+                    "foreign key(wordId) references Word(wordId) " +
+                    ")";
+                command = new SQLiteCommand(sql, connection);
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
