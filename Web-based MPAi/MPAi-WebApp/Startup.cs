1a97dfdf0861d66dc1b99905e004a86424d90c4b
Jayden Cooke <jaydencooke@outlook.com>
2017-08-21 18:53:26 +1200

    Fix error where audio files wouldn't play.

--- Web-based MPAi/MPAi-WebApp/Startup.cs
+++ Web-based MPAi/MPAi-WebApp/Startup.cs
@@ -8,6 +8,8 @@ namespace MPAi_WebApp
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
+            // Initialise database. This could take some time, so is called on startup.
+            MPAiSQLite initialDB = new MPAiSQLite();
             //using(MPAiContext context = MPAiContext.InitializeDBModel())
             //{
             //    // Force the database to be seeded on startup
