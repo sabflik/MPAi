9e11b1ca29c8c0bd47730dd88d9cc9ef42283d5d
sabflik <szaf045@aucklanduni.ac.nz>
2017-08-26 00:04:13 +1200

    Remove strict password requirement

--- Web-based MPAi/MPAi-WebApp/App_Start/IdentityConfig.cs
+++ Web-based MPAi/MPAi-WebApp/App_Start/IdentityConfig.cs
@@ -42,7 +42,7 @@ namespace MPAi_WebApp
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
-                AllowOnlyAlphanumericUserNames = false,
+                AllowOnlyAlphanumericUserNames = true,
                 RequireUniqueEmail = true
             };
 
@@ -50,10 +50,10 @@ namespace MPAi_WebApp
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
@@ -69,7 +69,7 @@ namespace MPAi_WebApp
             });
 
             // Configure user lockout defaults
-            manager.UserLockoutEnabledByDefault = true;
+            manager.UserLockoutEnabledByDefault = false;
             manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
             manager.MaxFailedAccessAttemptsBeforeLockout = 5;
 
