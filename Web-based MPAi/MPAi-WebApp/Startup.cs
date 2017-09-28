627dd35fb9dd1c1b22f783ff9df69dcfcc8a9a8c
JSCooke <jaydencooke@outlook.com>
2017-09-28 15:17:53 +1300

    Comment Startup class further

--- Web-based MPAi/MPAi-WebApp/Startup.cs
+++ Web-based MPAi/MPAi-WebApp/Startup.cs
@@ -7,6 +7,7 @@ namespace MPAi_WebApp
 {
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
+            // Set up authentication.
             ConfigureAuth(app);
             // Initialise database. This could take some time, so is called on startup.
             MPAiSQLite initialDB = new MPAiSQLite();
